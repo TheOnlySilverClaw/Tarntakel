7f44217a86746b37aa7bfdcf5b7efc7c10003a92
tigerplush <64077786+tigerplush@users.noreply.github.com>
2024-11-16 16:40:25 +0100

    added visibility cone size

--- Octo HSV/Assets/Scripts/DetectPlayer.cs
+++ Octo HSV/Assets/Scripts/DetectPlayer.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class DetectPlayer : MonoBehaviour
 {
+    [SerializeField]
+    private FloatAttribute _visibility;
+    [SerializeField]
+    private AnimationCurve _xScale;
+    [SerializeField]
+    private AnimationCurve _yScale;
+
+    private void Update()
+    {
+        float x = _xScale.Evaluate(_visibility.Value);
+        float y = _yScale.Evaluate(_visibility.Value);
+        transform.localScale = new Vector3(x, y, 1f);
+    }
     void OnCollisionStay2D(Collision2D collision) {
 
         GameObject target = collision.collider.gameObject;
