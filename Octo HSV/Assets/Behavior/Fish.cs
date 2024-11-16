06f980d913c2b1c9ec29979b9d15e2083201568b
Joel <silverclaw@noreply.codeberg.org>
2024-11-16 11:40:42 +0100

    Hai schwimmt

--- Octo HSV/Assets/Behavior/Fish.cs
+++ Octo HSV/Assets/Behavior/Fish.cs
@@ -5,35 +5,49 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject[] waypoints;
-    public float speed = 1.2f;
-    private int waypointIndex = 0;
+    public float speed = 5f;
+    private int waypointIndex;
     private Vector3 targetPosition;
+    private Vector3 localScale;
 
     // Start is called before the first frame update
     void Start()
     {
+        waypointIndex = 0;
+        localScale = transform.localScale;
         targetPosition = waypoints[0].transform.position;
+        UpdateTargetPosition();
     }
-
 
     // Update is called once per frame
     void Update()
     {
+
+        UpdateTargetPosition();
+        // Quaternion lookTarget = Quaternion.LookRotation(direction, Vector3.forward);
+        // Quaternion rotation = Quaternion.Slerp(
+        //     transform.rotation, lookTarget, 0.1f);
+        // transform.rotation = rotation;
+        transform.position = Vector3.MoveTowards(
+            transform.position, targetPosition, Time.deltaTime * speed);
+    }
+
+    void UpdateTargetPosition() {
+
         if(Vector3.Distance(transform.position, targetPosition) < 1.0f) {
             waypointIndex++;
             if(waypointIndex == waypoints.Length) {
                 waypointIndex = 0;
             }
             targetPosition = waypoints[waypointIndex].transform.position;
-        }
 
-        Vector3 direction = transform.position - targetPosition;
-        // Quaternion lookTarget = Quaternion.LookRotation(direction, Vector3.forward);
-        // Quaternion rotation = Quaternion.Slerp(
-        //     transform.rotation, lookTarget, 0.1f);
-        // transform.rotation = rotation;
-        transform.right = direction;
-        transform.position = Vector3.MoveTowards(
-            transform.position, targetPosition, Time.deltaTime * 0.5f);
+            Vector3 direction = transform.position - targetPosition;
+            transform.right = direction;
+
+            if(direction.y < 0) {
+                Vector3 flipped = new Vector3(localScale.x, -localScale.y, localScale.z);
+                transform.localScale = flipped;
+            }
+        }
     }
 }
