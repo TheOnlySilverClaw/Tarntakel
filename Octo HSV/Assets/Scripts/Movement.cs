a10d3ad1e1935edcda705282e7233d826a315f2c
Bakameisteru <Jakobusu@gmail.com>
2024-11-16 14:25:22 +0100

    huectopos looks towards the direction its going

---
+++ Octo HSV/Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _movementSpeed;
+    [SerializeField]
+    private Animator _animator;
+    private bool looksLeft;
+
+    /// <summary>
+    /// Movement intent is already normalized
+    /// </summary>
+    private Vector2 _movementIntent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        bool isMoving = _movementIntent.sqrMagnitude > 0.1f;
+        _animator.SetBool("isMoving", isMoving);
+        transform.position += new Vector3(_movementIntent.x, _movementIntent.y) * _movementSpeed * Time.deltaTime;
+        orientation();
+    }
+
+    public void Move(CallbackContext context)
+    {
+        _movementIntent = context.ReadValue<Vector2>();
+    }
+
+    private void orientation(){
+        Vector3 scale = transform.localScale;
+        if(!looksLeft && _movementIntent.x < 0){
+            scale.x *= -1;
+            looksLeft = true;
+        }
+        if(looksLeft && _movementIntent.x > 0){
+            scale.x *= -1;
+            looksLeft = false;
+        }
+        transform.localScale = scale;
+    }
+}
